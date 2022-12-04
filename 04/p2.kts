c9318599504e4c614e5c080313e856e577e10b73
Jellp <jellp55@gmail.com>
2022-12-04 10:53:47 +0100

    day 4

---
+++ 04/p2.kts
@@ -0,0 +1,20 @@
+import java.io.File
+
+val input = File("input").readLines()
+
+var result = 0
+
+for (line in input) {
+    val sections = line.split(',')
+    val f = sections[0].split('-').map { it.toInt() }
+    val s = sections[1].split('-').map { it.toInt() }
+    val firstSection = f[0]..f[1]
+    val secondSection = s[0]..s[1]
+
+    if (firstSection.any { secondSection.contains(it) }) {
+        result++
+        println("One of ${sections[0]} and ${sections[1]} contain one another")
+    }
+}
+
+println(result)
